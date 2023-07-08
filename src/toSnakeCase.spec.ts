63366ecbcbdba720dd84a89d484e7542973136ff
Jerridan Quiring <jerridan@gmail.com>
2023-07-08 15:01:10 -0400

    Handle nested keys

--- src/toSnakeCase.spec.ts
+++ src/toSnakeCase.spec.ts
@@ -6,4 +6,18 @@ describe("toSnakeCase", () => {
       object_key: "value",
     });
   });
+
+  it("converts nested keys", () => {
+    expect(
+      toSnakeCase({
+        objectKey: {
+          nestedKey: { deeplyNestedKey: "value" },
+        },
+      }),
+    ).toEqual({
+      object_key: {
+        nested_key: { deeply_nested_key: "value" },
+      },
+    });
+  });
 });
--- src/toSnakeCase.ts
+++ src/toSnakeCase.ts
@@ -1,7 +1,11 @@
 import snakeCase from "lodash.snakecase";
 
-export function toSnakeCase(obj: any) {
+export function toSnakeCase(obj: any): any {
   return Object.keys(obj).reduce((acc, value) => {
+    if (typeof obj[value] === "object") {
+      return { ...acc, [snakeCase(value)]: toSnakeCase(obj[value]) };
+    }
+
     return { ...acc, [snakeCase(value)]: obj[value] };
   }, {});
 }
