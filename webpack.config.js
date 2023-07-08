8f0dfde25e99348ba926594056412247e2f2e333
Jerridan Quiring <jerridan@gmail.com>
2023-07-08 14:00:47 -0400

    Fix name of build file

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const config = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
     library: {
       name: "convertKeys",
       type: "umd",
