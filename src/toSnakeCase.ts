aa3bcc96e17e1acce744263eb3b2527fb98a94cd
Jerridan Quiring <jerridan@gmail.com>
2023-07-08 15:04:20 -0400

    Refactor: rename parameter

--- src/toSnakeCase.ts
+++ src/toSnakeCase.ts
@@ -1,11 +1,11 @@
 import snakeCase from "lodash.snakecase";
 
 export function toSnakeCase(obj: any): any {
-  return Object.keys(obj).reduce((acc, value) => {
-    if (typeof obj[value] === "object") {
-      return { ...acc, [snakeCase(value)]: toSnakeCase(obj[value]) };
+  return Object.keys(obj).reduce((acc, key) => {
+    if (typeof obj[key] === "object") {
+      return { ...acc, [snakeCase(key)]: toSnakeCase(obj[key]) };
     }
 
-    return { ...acc, [snakeCase(value)]: obj[value] };
+    return { ...acc, [snakeCase(key)]: obj[key] };
   }, {});
 }
