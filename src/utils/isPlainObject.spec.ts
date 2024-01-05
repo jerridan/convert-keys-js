6844be83e7186cd080c1d0171745617d44f0ae20
Jerridan Quiring <jerridan@gmail.com>
2024-01-04 21:55:03 -0500

    Refactor: use isEqual instead of toBe

--- src/utils/isPlainObject.spec.ts
+++ src/utils/isPlainObject.spec.ts
@@ -2,42 +2,42 @@ import { isPlainObject } from "./isPlainObject";
 
 describe("isPlainObject", () => {
   it("returns true for plain objects", () => {
-    expect(isPlainObject({})).toBe(true);
-    expect(isPlainObject({ a: 1 })).toBe(true);
+    expect(isPlainObject({})).toEqual(true);
+    expect(isPlainObject({ a: 1 })).toEqual(true);
   });
 
   it("returns true when the constructor property has been changed", () => {
-    expect(isPlainObject({ constructor: 1 })).toBe(true);
+    expect(isPlainObject({ constructor: 1 })).toEqual(true);
   });
 
   it("returns false for objects with a prototype", () => {
-    expect(isPlainObject(Object.create({ a: 1 }))).toBe(false);
+    expect(isPlainObject(Object.create({ a: 1 }))).toEqual(false);
   });
 
   it("returns true for objects with a valueOf method", () => {
-    expect(isPlainObject({ valueOf: 0 })).toBe(true);
+    expect(isPlainObject({ valueOf: 0 })).toEqual(true);
   });
 
   it("returns false for arrays", () => {
-    expect(isPlainObject([])).toBe(false);
-    expect(isPlainObject([1, 2])).toBe(false);
+    expect(isPlainObject([])).toEqual(false);
+    expect(isPlainObject([1, 2])).toEqual(false);
   });
 
   it("returns false for functions", () => {
-    expect(isPlainObject(() => {})).toBe(false);
+    expect(isPlainObject(() => {})).toEqual(false);
   });
 
   it("returns false for primitives", () => {
-    expect(isPlainObject("string")).toBe(false);
-    expect(isPlainObject(1)).toBe(false);
-    expect(isPlainObject(true)).toBe(false);
+    expect(isPlainObject("string")).toEqual(false);
+    expect(isPlainObject(1)).toEqual(false);
+    expect(isPlainObject(true)).toEqual(false);
   });
 
   it("returns false for null", () => {
-    expect(isPlainObject(null)).toBe(false);
+    expect(isPlainObject(null)).toEqual(false);
   });
 
   it("returns false for undefined", () => {
-    expect(isPlainObject(undefined)).toBe(false);
+    expect(isPlainObject(undefined)).toEqual(false);
   });
 });
